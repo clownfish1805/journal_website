d374eb2f5cb115423550e32b59b0c2cb29debad2
akkshara <akkshara.ss2021cce@sece.ac.in>
2024-11-08 22:03:27 +0530

    responsiveness added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
+import "./Home.css";
 import Header from "../Components/Header/Header";
 import Footer from "../Components/Footer/Footer";
+import bg from "../Assets/bg.svg";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <div className="full-screen-bg">
+        <img src={bg}></img>
+      </div>
       <Footer />
     </>
   );
